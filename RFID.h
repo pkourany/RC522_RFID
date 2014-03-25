c45a26382c3e7411013f4e373d77be533c84a31e
Paul Kourany <pkourany@users.noreply.github.com>
2014-03-25 16:22:43 -0400

    Update RFID.h
    changed unsigned char to uint8_t and unsigned int to uint16_t

--- RFID.h
+++ RFID.h
@@ -15,33 +15,33 @@
 /******************************************************************************
  * Definitions
  ******************************************************************************/
-#define MAX_LEN 16   // Largo máximo de la matriz
+#define MAX_LEN 16   // Largo mÃ¡ximo de la matriz
 
 //MF522 comando palabra
 #define PCD_IDLE              0x00               // NO action; Y cancelar el comando
-#define PCD_AUTHENT           0x0E               // autenticación de clave
-#define PCD_RECEIVE           0x08               // recepción de datos
+#define PCD_AUTHENT           0x0E               // autenticaciÃ³n de clave
+#define PCD_RECEIVE           0x08               // recepciÃ³n de datos
 #define PCD_TRANSMIT          0x04               // Enviar datos
 #define PCD_TRANSCEIVE        0x0C               // Enviar y recibir datos
 #define PCD_RESETPHASE        0x0F               // reajustar
 #define PCD_CALCCRC           0x03               // CRC calcular
 
 //Mifare_One  Tarjeta Mifare_One comando palabra
-#define PICC_REQIDL           0x26               // Área de la antena no está tratando de entrar en el estado de reposo
-#define PICC_REQALL           0x52               // Todas las cartas para encontrar el área de la antena
-#define PICC_ANTICOLL         0x93               // anti-colisión
-#define PICC_SElECTTAG        0x93               // elección de tarjeta
-#define PICC_AUTHENT1A        0x60               // verificación key A
-#define PICC_AUTHENT1B        0x61               // verificación Key B
+#define PICC_REQIDL           0x26               // Ãrea de la antena no estÃ¡ tratando de entrar en el estado de reposo
+#define PICC_REQALL           0x52               // Todas las cartas para encontrar el Ã¡rea de la antena
+#define PICC_ANTICOLL         0x93               // anti-colisiÃ³n
+#define PICC_SElECTTAG        0x93               // elecciÃ³n de tarjeta
+#define PICC_AUTHENT1A        0x60               // verificaciÃ³n key A
+#define PICC_AUTHENT1B        0x61               // verificaciÃ³n Key B
 #define PICC_READ             0x30               // leer bloque
 #define PICC_WRITE            0xA0               // Escribir en el bloque 
 #define PICC_DECREMENT        0xC0               // cargo
 #define PICC_INCREMENT        0xC1               // recargar
 #define PICC_RESTORE          0xC2               // Transferencia de datos de bloque de buffer
-#define PICC_TRANSFER         0xB0               // Guardar los datos en el búfer
+#define PICC_TRANSFER         0xB0               // Guardar los datos en el bÃºfer
 #define PICC_HALT             0x50               // inactividad
 
-//MF522 Código de error de comunicación cuando regresó
+//MF522 CÃ³digo de error de comunicaciÃ³n cuando regresÃ³
 #define MI_OK                 0
 #define MI_NOTAGERR           1
 #define MI_ERR                2
@@ -120,37 +120,37 @@
 class RFID
 {
   public:
-    RFID(int chipSelectPin, int NRSTPD);
+    RFID(uint8_t chipSelectPin, uint8_t NRSTPD);
     
-    RFID(int chipSelectPin, int NRSTPD, uint8_t mosiPin, uint8_t misoPin, uint8_t clockPin);
+    RFID(uint8_t chipSelectPin, uint8_t NRSTPD, uint8_t mosiPin, uint8_t misoPin, uint8_t clockPin);
 
 	bool isCard();
 	bool readCardSerial();
 
     void init();
 	void reset();
-	void writeMFRC522(unsigned char addr, unsigned char val);
+	void writeMFRC522(uint8_t addr, uint8_t val);
 	void antennaOn(void);
-	unsigned char readMFRC522(unsigned char addr);
-	void setBitMask(unsigned char reg, unsigned char mask);
-	void clearBitMask(unsigned char reg, unsigned char mask);
-	void calculateCRC(unsigned char *pIndata, unsigned char len, unsigned char *pOutData);
-	unsigned char MFRC522Request(unsigned char reqMode, unsigned char *TagType);
-	unsigned char MFRC522ToCard(unsigned char command, unsigned char *sendData, unsigned char sendLen, unsigned char *backData, unsigned int *backLen);
-	unsigned char anticoll(unsigned char *serNum);
-	unsigned char auth(unsigned char authMode, unsigned char BlockAddr, unsigned char *Sectorkey, unsigned char *serNum);
-	unsigned char read(unsigned char blockAddr, unsigned char *recvData);
-	unsigned char write(unsigned char blockAddr, unsigned char *writeData);
+	uint8_t readMFRC522(uint8_t addr);
+	void setBitMask(uint8_t reg, uint8_t mask);
+	void clearBitMask(uint8_t reg, uint8_t mask);
+	void calculateCRC(uint8_t *pIndata, uint8_t len, uint8_t *pOutData);
+	uint8_t MFRC522Request(uint8_t reqMode, uint8_t *TagType);
+	uint8_t MFRC522ToCard(uint8_t command, uint8_t *sendData, uint8_t sendLen, uint8_t *backData, uint16_t *backLen);
+	uint8_t anticoll(uint8_t *serNum);
+	uint8_t auth(uint8_t authMode, uint8_t BlockAddr, uint8_t *Sectorkey, uint8_t *serNum);
+	uint8_t read(uint8_t blockAddr, uint8_t *recvData);
+	uint8_t write(uint8_t blockAddr, uint8_t *writeData);
 	void halt();
 
-	unsigned char serNum[5];       // Constante para guardar el numero de serie leido.
-	unsigned char AserNum[5];      // Constante para guardar el numero d serie de la secion actual.
+	uint8_t serNum[5];       // Constante para guardar el numero de serie leido.
+	uint8_t AserNum[5];      // Constante para guardar el numero d serie de la secion actual.
 
   private:
     uint8_t softSPITranser(uint8_t data);      // SOFT SPI
   
-    int _chipSelectPin;
-	int _NRSTPD;
+    uint8_t _chipSelectPin;
+	uint8_t _NRSTPD;
     uint8_t _mosiPin;
     uint8_t _misoPin;
     uint8_t _clockPin;
